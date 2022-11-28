8a58e94d4c6b224d33509d181e4dd9d2cbd5f7b5
Tim Veil <3260845+timveil@users.noreply.github.com>
2022-11-28 16:03:57 -0500

    updating readme and startup script

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -it -p 8080:8080 -p 26257:26257 -e "DATABASE_NAME=test" -e "MEMORY_SIZE=.5" timveil/cockroachdb-single-node:latest
+docker run -d -it -p 8080:8080 -p 26257:26257 -e "DATABASE_NAME=test" -e "MEMORY_SIZE=.5" --platform linux/amd64/v8 timveil/cockroachdb-single-node:latest
